026f1321a5d7ee62198ab96225206ad0abdcbdd8
Papan Namasudra <papannamasudra@ec2-16-16-9-203.eu-north-1.compute.amazonaws.com>
2025-04-02 14:41:41 +0530

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Papan Namasudra</h1>
+     <h3>phone: 9050XXXXXXX</h3>
+     <h3>insta: papan_079</h3>
+    </>
+  )
+}
+
+export default App
